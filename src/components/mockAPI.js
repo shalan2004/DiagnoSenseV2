611e6b645b5149353a0a74697c29d1bff7b5edd7
Shalan <shalan.2004@outlook.com>
2026-05-24 22:38:38 +0300

    implemented the refactored delete endpoint

--- src/components/mockAPI.js
+++ src/components/mockAPI.js
@@ -1,8 +1,8 @@
-const API_BASE_URL = 'https://nontelepathically-pamphletary-cyndi.ngrok-free.dev';
+// const API_BASE_URL = 'https://nontelepathically-pamphletary-cyndi.ngrok-free.dev';
 // const API_BASE_URL = 'https://toothlike-intermetatarsal-avah.ngrok-free.dev';
 // const API_BASE_URL = 'https://unpersecuted-vanitied-jayson.ngrok-free.dev';
 // const API_BASE_URL = 'https://unallegedly-wrinkly-claribel.ngrok-free.dev';
-// const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'http://127.0.0.1:8000';
 
 import { getCookie, setCookie, deleteCookie, setJsonCookie } from './cookieUtils';
 
@@ -693,7 +693,7 @@ export const getPatientNextVisitAPI = async (patientId) => {
 };
 
 export const deletePatientAPI = async (patientId) => {
-  return await apiCall(`/api/patients/${patientId}`, {
+  return await apiCall(`/api/v1/patients/${patientId}`, {
     method: 'DELETE',
   });
 };
